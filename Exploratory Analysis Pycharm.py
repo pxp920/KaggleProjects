58adfe78c18d981c3253405cde30fc2bf58aee07
Panos Pantazis <30659989+pxp920@users.noreply.github.com>
2017-10-27 14:44:53 -0400

    Edits
    Added Jupyter Notebook + Other Pycharm Scripts

--- Exploratory Analysis Pycharm.py
+++ Exploratory Analysis Pycharm.py
@@ -544,3 +544,48 @@ fulltestpath =testfilepath+'/'+testfile
 matplotlib.pyplot.show(plot_image(fulltestpath))
 #, frames=(0,data.shape[2])
 
+# ----------------------------------------------------------------------------------------
+# get_hit_rate_stats(infile):  gets the threat probabilities in a useful form
+#
+# infile:                      labels csv file
+#
+# returns:                     a dataframe of the summary hit probabilities
+#
+# ----------------------------------------------------------------------------------------
+
+def get_hit_rate_stats(infile):
+    # pull the labels for a given patient
+    df = pd.read_csv(infile)
+
+    # Separate the zone and patient id into a df
+    df['Subject'], df['Zone'] = df['Id'].str.split('_', 1).str
+    df = df[['Subject', 'Zone', 'Probability']]
+
+    # make a df of the sums and counts by zone and calculate hit rate per zone, then sort high to low
+    df_summary = df.groupby('Zone')['Probability'].agg(['sum', 'count'])
+    df_summary['Zone'] = df_summary.index
+    df_summary['pct'] = df_summary['sum'] / df_summary['count']
+    df_summary.sort_values('pct', axis=0, ascending=False, inplace=True)
+
+    return df_summary
+
+
+# unit test -----------------------
+df = get_hit_rate_stats(THREAT_LABELS)
+df.head()
+
+
+#------------------------------------------------------------------------------------------
+# chart_hit_rate_stats(df_summary): charts threat probabilities in desc order by zone
+#
+# df_summary:                 a dataframe like that returned from get_hit_rate_stats(...)
+#
+#-------------------------------------------------------------------------------------------
+
+def chart_hit_rate_stats(df_summary):
+    fig, ax = plt.subplots(figsize=(15,5))
+    sns.barplot(ax=ax, x=df_summary['Zone'], y=df_summary['pct']*100)
+
+# unit test ------------------
+chart_hit_rate_stats(df)
+plt.show()
